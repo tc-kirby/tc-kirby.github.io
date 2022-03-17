816b3318c399cd430fc77b7d55fa57316cf2fbdc
tc-kirby <tc.kirby@gmail.com>
2022-03-17 21:09:28 +0000

    second commit

---
+++ slides.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    showSlides();
+});
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
